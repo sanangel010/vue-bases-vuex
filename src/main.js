f516c508b18412a3a5d131620f051129dc7495d8
Angel Haro <sanangel010@gmail.com>
2022-11-02 17:23:03 -0600

    Proyecto con las bases para el manejo de Vuex

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Si se pone asi en raíz busca el index.js, por lo cual ya no es necesario porlo asi: @/store/index.js
+import store from '@/store'
+
+
+// se aloja en el  nivel más alto 
+createApp(App)
+    .use( store )
+        .mount('#app')
